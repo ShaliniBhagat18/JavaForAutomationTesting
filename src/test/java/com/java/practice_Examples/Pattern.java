b8802cc96cd487e25d75448e76b5c7812e61477e
Shalini <SMBhagat@landolakes.com>
2024-02-09 17:58:32 +0530

    Completed Basic Java Programming

---
+++ src/test/java/com/java/practice_Examples/Pattern.java
@@ -0,0 +1,24 @@
+package com.java.practice_Examples;
+
+public class Pattern {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("* *");
+		System.out.println("* * *");
+		System.out.println("* * * *");
+		System.out.println("* * * * *");
+
+		System.out.println("* * * * *");
+		System.out.println("* * * *");
+		System.out.println("* * *");
+		System.out.println("* *");
+		System.out.println("*");
+
+		System.out.println("        *");
+		System.out.println("      * *");
+		System.out.println("    * * *");
+		System.out.println("  * * * *");
+		System.out.println("* * * * *");
+
+	}
+}
---
+++ src/test/java/com/java/practice_Examples/PositiveNegativeZero.java
@@ -0,0 +1,21 @@
+package com.java.practice_Examples;
+
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to Number checker\n");
+		System.out.print("Please enter your number: ");
+		int num = input.nextInt();
+
+		if (num > 0) {
+			System.out.println("Your number is positive");
+		} else if (num == 0) {
+			System.out.println("Your number is zero");
+		} else {
+			System.out.println("Your number is negative");
+		}
+	}
+}
